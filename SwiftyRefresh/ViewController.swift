1a31ab34ee02ab1b5389414f734d06b3ffe884ab
Michael Ackley <ackleymi@gmail.com>
2015-04-30 23:21:26 -0400

    Initial Commit

---
+++ SwiftyRefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyRefresh
+//
+//  Created by Mike Ackley on 4/30/15.
+//  Copyright (c) 2015 Michael Ackley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
